0c1f408ef8f1e4a73e966f4161b91d998d2ce537
Rajvardhan Singh <vardhanrajsingh04@gmail.com>
2025-03-07 01:02:55 +0530

    lecture - 22

--- lib/main.dart
+++ lib/main.dart
@@ -693,7 +693,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
        */
-
       /* lecture 21
       ElevatedButton(
         onPressed: callBack,
@@ -702,7 +701,87 @@ class _MyHomePageState extends State<MyHomePage> {
 
        */
 
+    /* lecture - 22
+      Container(
+        child: Column(
+          children: [
+            topItems(),
 
+            Expanded(
+              flex: 4,
+              child: Container(
+                color: Colors.orange,
+                child: ListView.builder(
+                  itemBuilder:
+                      (context, index) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: CircleAvatar(backgroundColor: Colors.green),
+                          title: Text('Name'),
+                          subtitle: Text('Mobile No'),
+                          trailing: Icon(Icons.delete),
+                        ),
+                      ),
+                ),
+              ),
+            ),
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.brown,
+                child: ListView.builder(
+                  itemBuilder:
+                      (context, index) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(11),
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                  itemCount: 100,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ),
+
+            Expanded(flex: 2, child: Container(color: Colors.green)),
+          ],
+        ),
+      ),
+
+     */
     );
   }
+}
+
+/*
+class topItems extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: Colors.blueAccent,
+        child: ListView.builder(
+          itemBuilder:
+              (context, index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 100,
+              child: CircleAvatar(backgroundColor: Colors.green),
+            ),
+          ),
+          itemCount: 20,
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
+ */
+
+
 }
